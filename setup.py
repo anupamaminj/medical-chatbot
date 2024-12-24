d264634ffef0534505a3742347522d52b9dd1e37
Your Name <you@example.com>
2024-12-24 17:03:45 +0530

    folder created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative ai project',
+    version = '0.0.0.0',
+    author = 'anupama',
+    author_email = 'ruby.13may2000@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
